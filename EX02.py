c17e702acce58b90b74aeb31a1ba50108d138706
Larissa M Loose <l.m.loose22@gmail.com>
2025-11-16 21:52:32 -0300

    Aulas

---
+++ EX02.py
@@ -0,0 +1,5 @@
+nome='Larissa'
+idade='29 anos'
+peso='40.5 kg'
+print(nome, idade, peso)
+
---
+++ EX04.py
@@ -0,0 +1,9 @@
+'''
+nome=input('Digite seu nome: ')
+print('É um prazer te conhecer!', nome)
+'''
+
+#Para o python usa formatação para usar basta usar o seguinte comando: {}.format
+nome=input("Digite seu nome: ")
+print("É um prazer te conhecer, {}!".format(nome))
+
